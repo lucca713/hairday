23558a0019af32a3f46be3aeafa4205dff17c26d
Lucca <cerasommalucca@gmail.com>
2025-05-15 08:32:25 -0300

    integrando o html e o css no modulo do webpack

---
+++ hairday-template-main/src/main.js
@@ -0,0 +1,6 @@
+"use strict"
+ 
+//importar o CSS
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     target: "web",
@@ -8,7 +9,31 @@ module.exports = {
 
     output:{
         filename:"main.js",
-        path: path.resolve(__dirname,"dist")
-    }
+        path: path.resolve(__dirname,"dist") //aonde ele vai gravar tudo
+    },
 
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")       
+         },
+         port: 3000,
+         open: true,
+         liveReload: true,
+    },
+
+    plugins: [ 
+        
+        new HtmlWebpackPlugin({ 
+            template: path.resolve(__dirname,"hairday-template-main","index.html"),
+            favicon: path.resolve("hairday-template-main","src","assets","scissors.svg"),   
+        }),
+    
+    ],
+
+    module:{
+        rules:[{
+            test: /\.css$/,
+            user: ["style-loader","css-loader"],
+        }],
+    },
 }
