336aa63163c5c272e8c7a84ad8199e3bfbf1d53b
Lucca <cerasommalucca@gmail.com>
2025-05-23 08:26:51 -0300

    fim da incrementacao da funcao date

--- hairday-template-main/src/modules/form/hours-load.js
+++ hairday-template-main/src/modules/form/hours-load.js
@@ -1,8 +1,27 @@
 //importar os horarios de abertura
 import { openingHours } from "../../utils/opening_hours.js"
+import dayjs from "dayjs";
 
-export function hoursLoad(){
+export function hoursLoad({ date }){
+
     const opening = openingHours.map((hours) =>{
-        console.log(hours)
-    } )
+        const [scheduleHour] = hours.split(":")
+        
+
+        //adicionar a hora nas datas e verificar se esta no passado
+
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+        
+
+        //definir se o horario esta vago
+
+        
+
+        return{
+            scheduleHour,
+            available : isHourPast,
+        }
+    })
+
+    console.log(opening)
 }
--- hairday-template-main/src/modules/schedules/load.js
+++ hairday-template-main/src/modules/schedules/load.js
@@ -1,8 +1,12 @@
 import {hoursLoad} from "../form/hours-load.js"
 
+//Seleciona a hora do input de data
+const selectedDate = document.getElementById("date")
+
 export function schedulesDays(){
     //renderiza as horas disponiveis.
 
-    hoursLoad()
+    const date = selectedDate.value
+    hoursLoad(date)
     
 }
