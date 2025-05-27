f7a03b1196052202d78844da8b841a73e58f4222
Lucca <cerasommalucca@gmail.com>
2025-05-27 08:14:04 -0300

    configurando a classe horas

--- hairday-template-main/src/modules/form/hours-load.js
+++ hairday-template-main/src/modules/form/hours-load.js
@@ -2,26 +2,33 @@
 import { openingHours } from "../../utils/opening_hours.js"
 import dayjs from "dayjs";
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }){
 
     const opening = openingHours.map((hours) =>{
         const [scheduleHour] = hours.split(":")
         
-
+        console.log(hours)
         //adicionar a hora nas datas e verificar se esta no passado
 
         const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
         
 
         //definir se o horario esta vago
-
         
-
         return{
             scheduleHour,
             available : isHourPast,
         }
     })
-
     console.log(opening)
+    opening.forEach(({hour, available})=>{
+        const li = document.createElement("li")
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+        hours.append(li)
+    })
 }
