2ab2177705d2f33686b46042dfff61f2af023ea7
Lucca <cerasommalucca@gmail.com>
2025-05-29 08:24:49 -0300

    Renderizando de acordo com o relógio e separando os períodos

--- hairday-template-main/src/modules/form/hours-load.js
+++ hairday-template-main/src/modules/form/hours-load.js
@@ -6,29 +6,45 @@ const hours = document.getElementById("hours")
 
 export function hoursLoad({ date }){
 
-    const opening = openingHours.map((hours) =>{
-        const [scheduleHour] = hours.split(":")
+    const opening = openingHours.map((hour) =>{
+    
+        const [scheduleHour] = hour.split(":") //quebra a list ade horas no : 
         
-        console.log(hours)
-        //adicionar a hora nas datas e verificar se esta no passado
-
-        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+        
+        //devolve um bool para cada hora de acordo com o relogio
+        const isHourFuture = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
         
 
         //definir se o horario esta vago
-        
-        return{
-            scheduleHour,
-            available : isHourPast,
+        return{   
+            hour,
+            available : isHourFuture,
         }
     })
-    console.log(opening)
+    //console.log(opening)
     opening.forEach(({hour, available})=>{
         const li = document.createElement("li")
         li.classList.add("hour")
         li.classList.add(available ? "hour-available" : "hour-unavailable")
 
         li.textContent = hour
+
+        if(hour === "9:00"){
+            hourHeaderAdd("Manhã")
+        } else if(hour === "13:00"){
+            hourHeaderAdd("Tarde")
+        }else if(hour === "18:00"){
+            hourHeaderAdd("Noite")
+        }
+
         hours.append(li)
     })
+}
+
+function hourHeaderAdd(title){
+    const header = document.createElement("li")
+    header.classList.add("hour-period")
+    header.textContent = title
+
+    hours.append(header) // se der bo é aqui nas horas 
 }
